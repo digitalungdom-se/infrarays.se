f727300f87f312fe14c6fa6377192354f1ac9343
Nautman <douglas@digitalungdom.se>
2019-12-29 13:23:50 +0100

    Initial commit

---
+++ src/components/Plate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default ({children}) => (
+  <div
+    style={{
+      position: 'absolute', left: '50%', top: '50%',
+      transform: 'translate(-50%, -50%)'
+    }}
+  >
+    {children}
+  </div>
+);
