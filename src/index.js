a11e26972cf0300e82913f8ced7ba2e442a4ce38
Nautman <douglas@digitalungdom.se>
2020-01-23 22:13:13 +0100

    Squashed eslint errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App.jsx';
+import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
