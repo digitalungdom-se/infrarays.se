2cd27cea7139174b717032be3b3bafdd4553d033
Nautman <douglas@digitalungdom.se>
2020-01-30 13:51:35 +0100

    Fixed persist bug

--- src/store.js
+++ src/store.js
@@ -1,5 +1,18 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware
+} from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import app from 'features/appSlice';
 
@@ -15,7 +28,12 @@ const rootReducer = combineReducers({
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    }
+  })
 });
 
 export const persistor = persistStore(store);
