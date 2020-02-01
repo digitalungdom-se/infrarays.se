2809b5b3d852d5260f22d32dcd36976683c8245d
Nautman <douglas@digitalungdom.se>
2020-02-01 15:30:08 +0100

    Pushilush: : The Next Level

--- src/features/recommendation/index.jsx
+++ src/features/recommendation/index.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import CenterCard from 'components/CenterCard';
-import { Form, Alert } from 'react-bootstrap';
+import { Form, Alert, Spinner } from 'react-bootstrap';
 import Upload from 'components/portal/Upload';
 import { useParams } from 'react-router-dom';
 import useFetch from 'utils/useFetch';
 import { useTranslation } from 'react-i18next';
 
-const UploadState = ({ setSuccess }) => {
+const esc = encodeURIComponent;
+export const query = params =>
+  Object.keys(params)
+    .map(k => `${esc(k)}=${esc(params[k])}`)
+    .join('&');
+
+const UploadState = ({ name, setSuccess }) => {
+  const [error, setError] = useState();
   const [uploading, setUploading] = useState();
   const [uploaded, setUploaded] = useState();
   const { userID, recommendationID } = useParams();
@@ -15,8 +22,13 @@ const UploadState = ({ setSuccess }) => {
     <Upload
       label="Ladda upp rekommendationsbrev"
       uploading={uploading}
-      uploaded={uploaded}
+      uploaded={name || error?.fileName}
+      error={error?.msg}
       onChange={(file, fileName) => {
+        if (file.size > 7 * Math.pow(10, 6)) {
+          setError({ msg: 'too large', fileName });
+          return;
+        }
         const body = new FormData();
         body.append('file', file, fileName);
         setUploading(true);
@@ -40,60 +52,75 @@ const UploadState = ({ setSuccess }) => {
 
 const Recommendation = () => {
   const [success, setSuccess] = useState();
-  const [error, setError] = useState();
+  // const [error, setError] = useState();
   const { userID, recommendationID } = useParams();
 
-  const [requested, setRequest] = useState(false);
-  const [response, setResponse] = useState();
-  function doFetch() {
-    setRequest(true);
-    console.log(requested);
-    fetch('/api/user/recommendation', {
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ userID, recommendationID })
-    })
-      .then(res => res.json())
-      .then(res => {
-        if (res.type === 'success') {
-          setResponse(res);
-        } else {
-          res.json = true;
-          throw res;
-        }
-      })
-      .catch(err => {
-        setError(err);
-      });
-  }
+  const { response, error, isLoading } = useFetch(
+    `/api/user/recommendation?${query({ userID, recommendationID })}`
+  );
 
-  if (!requested) doFetch();
+  let name = '';
+  if (response?.type === 'success') {
+    name = response.recommendationInfo.name;
+  }
 
   return (
     <CenterCard maxWidth="480px" title="Ladda upp rekommendationsbrev">
-      <Form>
-        <h4>Rekommendationsbrev för</h4>
-        {error !== undefined && (
-          <Alert variant="danger">
-            Kunde inte hitta någon elev som tillhör denna länk
-          </Alert>
-        )}
-        {error === undefined && (
-          <>
-            <p>
-              Tack för att du vill skriva ett rekommendationsbrev!
-              Rekommendationsbrevet skall skrivas, signeras och skickas av
-              elevens lärare, tränare eller liknande. Max 1 sida per brev.
-              Eleven kommer inte att kunna se brevet, men kommer att få
-              notifikation när det är uppladdat. Det får max vara 5 MB.
-            </p>
-            <UploadState setSuccess={setSuccess} />
-            {success && <Alert variant="success">Du är klar!</Alert>}
-          </>
-        )}
-      </Form>
+      {isLoading ? (
+        <Spinner
+          animation="border"
+          size="lg"
+          variant="custom"
+          style={{
+            width: '5rem',
+            height: '5rem',
+            fontSize: '2.5rem',
+            margin: '1rem auto',
+            display: 'block'
+          }}
+        />
+      ) : (
+        <Form>
+          <h4>
+            För{' '}
+            {name &&
+              name
+                .split(' ')
+                .map(n => n[0].toUpperCase() + n.substring(1, n.length))
+                .join(' ')}
+          </h4>
+          {error !== null && (
+            <Alert variant="danger">
+              Kunde inte hitta någon elev som tillhör denna länk
+            </Alert>
+          )}
+          {error === null && (
+            <>
+              <p>
+                Tack för att du vill skriva ett rekommendationsbrev!
+                Rekommendationsbrevet skall skrivas, signeras och skickas av
+                elevens lärare, tränare eller liknande. Max 1 sida per brev.
+                Eleven kommer inte att kunna se brevet, men kommer att få
+                notifikation när det är uppladdat. Det får max vara 5 MB.
+              </p>
+              {(response?.recommendationInfo.fileName || success) && (
+                <Alert variant="success">
+                  Du har laddat upp:{' '}
+                  <b>{response?.recommendationInfo.fileName}</b>
+                  <br />
+                  Du är klar!
+                </Alert>
+              )}
+              {response?.recommendationInfo.fileName === undefined && (
+                <UploadState
+                  name={response?.recommendationInfo.fileName}
+                  setSuccess={setSuccess}
+                />
+              )}
+            </>
+          )}
+        </Form>
+      )}
     </CenterCard>
   );
 };
