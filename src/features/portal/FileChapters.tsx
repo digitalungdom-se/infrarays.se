8e4353f542938238b47a5d0a9e9bb670a93506e1
Nautman <28629647+Nautman@users.noreply.github.com>
2021-04-05 15:25:21 +0200

    Bug fix to FileChapters

--- src/features/portal/FileChapters.tsx
+++ src/features/portal/FileChapters.tsx
@@ -4,16 +4,19 @@ import TranslatedChapter from "./TranslatedChapter";
 import Upload from "features/files/Upload";
 import portal from "config/portal.json";
 
-const FileChapters = (): React.ReactElement[] =>
-  portal.chapters
-    .filter((chapter) => chapter.upload !== undefined)
-    .map((chapter) => (
-      <TranslatedChapter key={chapter.fileType} type={chapter.fileType}>
-        <Upload
-          accept={chapter.upload?.accept}
-          fileType={chapter.fileType as FileType}
-          multiple={chapter.upload?.multiple}
-        />
-      </TranslatedChapter>
-    ));
+const FileChapters = (): React.ReactElement => (
+  <>
+    {portal.chapters
+      .filter((chapter) => chapter.upload !== undefined)
+      .map((chapter) => (
+        <TranslatedChapter key={chapter.fileType} type={chapter.fileType}>
+          <Upload
+            accept={chapter.upload?.accept}
+            fileType={chapter.fileType as FileType}
+            multiple={chapter.upload?.multiple}
+          />
+        </TranslatedChapter>
+      ))}
+  </>
+);
 export default FileChapters;
