ab29bd0df8fce42dc79d98cc3281dc9e02cb0e87
Nautman <28629647+Nautman@users.noreply.github.com>
2021-04-05 15:16:01 +0200

    Added comments to filesSlice

--- src/features/files/filesSlice.ts
+++ src/features/files/filesSlice.ts
@@ -3,8 +3,10 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 import { RootState } from "store";
 
+// Files are organized by their FileType, i.e. CV: [file1, file2, etc]
 type FilesByType = Partial<Record<FileType, FileInfo[]>>;
 
+// File types are organized by the application IDs, i.e. [user1_ID]: {CV: files}
 interface FilesState {
   fileTypesByApplicants: Record<string, FilesByType>;
 }
@@ -18,28 +20,38 @@ const filesSlice = createSlice({
   initialState,
   reducers: {
     setFiles(state, action: PayloadAction<FileInfo[]>) {
+      // loop through each file
       action.payload.forEach((file) => {
+        // if no files have been added to user, create an empty object
         if (state.fileTypesByApplicants[file.userId] === undefined) {
           state.fileTypesByApplicants[file.userId] = {};
         }
+        // add the file to the store
         if (
+          // if there are already uploaded files for this FileType
           state.fileTypesByApplicants[file.userId][file.type] &&
+          // and if the file is NOT already in there
           state.fileTypesByApplicants[file.userId][file.type]?.findIndex(
             (f) => f.id === file.id
           ) === -1
         ) {
+          // add the file to the array
           state.fileTypesByApplicants[file.userId][file.type]?.push(file);
-        } else state.fileTypesByApplicants[file.userId][file.type] = [file];
+        } // otherwise create a new array for the file
+        else state.fileTypesByApplicants[file.userId][file.type] = [file];
       });
     },
     replaceFile(state, action: PayloadAction<FileInfo>) {
       const file = action.payload;
+      // replace the array for the files - assuming only one file should exist
       state.fileTypesByApplicants[file.userId][file.type] = [file];
     },
     uploadSuccess(state, action: PayloadAction<FileInfo>) {
       const file = action.payload;
       const files = state.fileTypesByApplicants[file.userId][file.type];
+      // if there are files, add it to the array
       if (files) files.push(file);
+      // otherwise create a new array
       else state.fileTypesByApplicants[file.userId][file.type] = [file];
     },
     deleteFileSuccess(
@@ -67,8 +79,11 @@ export const {
 export const selectApplicantFilesLoaded = (applicantID?: string) => (
   state: RootState
 ): boolean => {
+  // if there is no id defined, use the userID
   const id = applicantID || state.auth.user?.id;
+  // if there is no id then there are no files
   if (!id) return false;
+  // check if there are files
   const fileTypesByApplicants = state.files.fileTypesByApplicants[id];
   return Boolean(fileTypesByApplicants);
 };
@@ -78,9 +93,12 @@ export const selectFilesByFileTypeAndApplicant = (
   applicantID?: string
 ) => (state: RootState): FileInfo[] => {
   const id = applicantID || state.auth.user?.id;
+  // no id -> no files
   if (!id) return [];
   const fileTypes = state.files.fileTypesByApplicants[id];
+  // if there are files, get the relevant files by the type. undefined -> no files
   if (fileTypes) return fileTypes[type] || [];
+  // catch all
   return [];
 };
 
