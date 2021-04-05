cebfb45530713c53ddf2bc4c0912ba9aedbcee79
Nautman <28629647+Nautman@users.noreply.github.com>
2021-04-05 14:43:31 +0200

    Changed FileChapters into TranslatedChapters to improve resuability

--- src/features/portal/FileChapters.tsx
+++ src/features/portal/FileChapters.tsx
@@ -1,34 +1,23 @@
-import { WithTranslation, withTranslation } from "react-i18next";
-
-import Chapter from "components/Chapter";
 import { FileType } from "types/files";
 import React from "react";
+import TranslatedChapter from "./TranslatedChapter";
 import Upload from "features/files/Upload";
 import portal from "config/portal.json";
 import { useFiles } from "features/files/filesHooks";
 
-const FileChapters = ({ t }: WithTranslation): React.ReactElement => {
+const FileChapters = (): React.ReactElement[] => {
   const { loading } = useFiles();
-  return (
-    <>
-      {portal.chapters
-        .filter((chapter) => chapter.upload !== undefined)
-        .map((chapter) => (
-          <Chapter
-            key={chapter.fileType}
-            title={t(`${chapter.fileType}.title`)}
-            description={t(`${chapter.fileType}.description`)}
-            subtitle={t(`${chapter.fileType}.subtitle`)}
-          >
-            <Upload
-              accept={chapter.upload?.accept}
-              fileType={chapter.fileType as FileType}
-              disabled={loading}
-              multiple={chapter.upload?.multiple}
-            />
-          </Chapter>
-        ))}
-    </>
-  );
+  return portal.chapters
+    .filter((chapter) => chapter.upload !== undefined)
+    .map((chapter) => (
+      <TranslatedChapter key={chapter.fileType} type={chapter.fileType}>
+        <Upload
+          accept={chapter.upload?.accept}
+          fileType={chapter.fileType as FileType}
+          disabled={loading}
+          multiple={chapter.upload?.multiple}
+        />
+      </TranslatedChapter>
+    ));
 };
-export default withTranslation()(FileChapters);
+export default FileChapters;
