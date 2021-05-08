755f895b400624ec8a9aef37d1156c12b013f001
Nautman <28629647+Nautman@users.noreply.github.com>
2021-05-08 22:47:54 +0200

    Converted Chapter story to TypeScript

--- src/components/Chapter/index.stories.tsx
+++ src/components/Chapter/index.stories.tsx
@@ -1,22 +1,11 @@
 import Chapter from "./index";
-import Plate from "components/Plate";
 import React from "react";
-import { UploadHook } from "components/Upload/index.stories";
 
 export default {
   title: "Chapter",
-  decorators: [
-    (Story) => (
-      <div style={{ background: "#f6f6f6", padding: 50 }}>
-        <Plate>
-          <Story />
-        </Plate>
-      </div>
-    ),
-  ],
 };
 
-export const withText = () => (
+export const withText = (): React.ReactElement => (
   <Chapter
     title="Personligt brev"
     subtitle="Max 600 ord"
@@ -25,6 +14,5 @@ export const withText = () => (
       "I ditt personliga brev vill vi därför att du kortfattat berättar om dina intressen och varför du söker till Rays." +
       "För oss är det intressant att höra varifrån din passion för naturvetenskap kommer och hur dina tidigare erfarenheter har påverkat dig. "
     }
-    upload={<UploadHook title="Ladda upp personligt brev" />}
   />
 );
