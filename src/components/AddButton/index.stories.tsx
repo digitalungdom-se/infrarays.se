64443838d614a657ac7d8399c0c630edbb3e7e1b
Nautman <28629647+Nautman@users.noreply.github.com>
2021-05-10 00:07:21 +0200

    Converted AddButton story to TypeScript

---
+++ src/components/AddButton/index.stories.tsx
@@ -0,0 +1,8 @@
+import AddButton from "./index";
+import React from "react";
+
+export default { title: "AddButton" };
+
+export const Basic = (): React.ReactElement => (
+  <AddButton>Add something</AddButton>
+);
