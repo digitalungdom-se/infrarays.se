de5e59e807662cccd0ec98dfdd01d8dad117db73
Nautman <28629647+Nautman@users.noreply.github.com>
2021-05-21 17:42:57 +0200

    Convert Star component to TypeScript and renamed it to AnimatedStar

--- src/components/AnimatedStar.tsx
+++ src/components/AnimatedStar.tsx
@@ -1,23 +1,21 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const Star = styled.label`
+const AnimatedStar = styled.label`
   color: ${(props) => props.theme.brand};
   font-family: sans-serif;
   animation: scale 5s infinite;
 
   @keyframes scale {
     0% {
-      transform: scale(1)
+      transform: scale(1);
     }
-
     50% {
       transform: scale(1.3);
     }
-
     100% {
-      transform: scale(1)
+      transform: scale(1);
     }
   }
 `;
 
-export default Star;
+export default AnimatedStar;
--- src/components/NoMatch.tsx
+++ src/components/NoMatch.tsx
@@ -1,7 +1,7 @@
+import AnimatedStar from "./AnimatedStar";
 import Center from "./Center";
 import Plate from "./Plate";
 import React from "react";
-import Star from "./Star";
 import StyledTitle from "./StyledTitle";
 import { Trans } from "react-i18next";
 
@@ -10,7 +10,7 @@ const NoMatch = (): React.ReactElement => (
     <Plate>
       <StyledTitle>
         404
-        <Star>*</Star>
+        <AnimatedStar>*</AnimatedStar>
       </StyledTitle>
       <hr />
       <Trans i18nKey="Not found">No page was found on this URL.</Trans>
