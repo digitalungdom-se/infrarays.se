2d8cc8bddcd25f6f303ffc31946128c7ec6468b2
Nautman <28629647+Nautman@users.noreply.github.com>
2021-05-21 18:11:05 +0200

    Converted service worker code to TypeScript
    Code is taken from: https://github.com/cra-template/pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-/* eslint-disable react/jsx-filename-extension */
-import React from "react";
-import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./i18n";
+
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 import App from "./App";
-// import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
