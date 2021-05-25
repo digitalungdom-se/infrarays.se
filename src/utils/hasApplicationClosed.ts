53ae5e5bf18fa1202d5f6217debd34336f4a8435
Nautman <28629647+Nautman@users.noreply.github.com>
2021-05-25 11:45:07 +0200

    Added deadline to config

--- src/utils/hasApplicationClosed.ts
+++ src/utils/hasApplicationClosed.ts
@@ -1,6 +1,5 @@
-import moment from "moment";
+import { deadline } from "config/portal.json";
 
-const hasApplicationClosed = (): boolean =>
-  moment.utc().month(2).endOf("month").diff(Date.now()) < 0;
+const hasApplicationClosed = (): boolean => deadline < Date.now();
 
 export default hasApplicationClosed;
