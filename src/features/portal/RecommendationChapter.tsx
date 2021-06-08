13f2463bd15777d79b193b44b7f37930cfea45a3
Nautman <28629647+Nautman@users.noreply.github.com>
2021-06-09 01:16:46 +0200

    Fixed bug
    The "max" prop in RecommendationChapter did not have a default value

--- src/features/portal/RecommendationChapter.tsx
+++ src/features/portal/RecommendationChapter.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Reference from "features/recommendations/Reference";
 import TranslatedChapter from "./TranslatedChapter";
 
+type RecommendationChapterProps = {
+  max?: number;
+};
+
 const RecommendationChapter = ({
-  max,
-}: {
-  max: number;
-}): React.ReactElement => {
+  max = 3,
+}: RecommendationChapterProps): React.ReactElement => {
   const map = [];
   for (let i = 0; i < max; i += 1) {
     map[i] = <Reference key={`email-person-${i}`} index={i} />;
